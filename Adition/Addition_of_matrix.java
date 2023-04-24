7507a55cccad7ef60fd1ecc894c2839060252a04
Anamuddin Ahmad <119073478+AnamuddinAhmad@users.noreply.github.com>
2023-04-25 00:33:00 +0530

    Create Addition_of_matrix

---
+++ Adition/Addition_of_matrix.java
@@ -0,0 +1,33 @@
+/*This is the code of adding two matrix
+*/
+public class practic{
+public static void main(String[] args) {
+Scanner sc= new Scanner(System.in);
+int mat1[][]={{1,2,3},
+{4,5,6}};
+int mat2[][]={{7,8,9},
+{7,6,5}};
+int result[][]={{0,0,0},
+{0,0,0}};
+for(int i=0;i<mat1.length;i++)
+{
+for(int j=0;j<mat1[i].length;j++)
+{
+// System.out.printf("set value for i=%d and j=%d \n",i,j);
+result[i][j]=mat1[i][j]+mat2[i][j];
+//Using one for loop for printing and adding matrix !
+System.out.print(result[i][j]+" ");
+}
+System.out.print("\n");
+}
+for(int i=0;i<mat1.length;i++)
+{
+for(int j=0;j<mat1[i].length;j++)
+{
+System.out.print(result[i][j] + " ");
+result[i][j]=mat1[i][j]+mat2[i][j];
+}
+System.out.println("\n");
+}
+}
+}
