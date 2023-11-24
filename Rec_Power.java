30659228df9e08939361f3c02714721e75d32319
Anamuddin Ahmad <119073478+AnamuddinAhmad@users.noreply.github.com>
2023-11-24 18:52:46 +0530

    Update Rec_Power.java

--- Rec_Power.java
+++ Rec_Power.java
@@ -2,9 +2,19 @@ public class Rec_Power {
     public static void main(String[] args) {
         int num=2;
         int pow=10;
+        System.out.println(power(num,pow));
         System.out.println(calculatePower(num,pow,1));
     }
 
+     public static int power(int num,int pow){
+        if (pow ==0) {
+            return 1;
+        }
+        int nm1=power(num, pow-1);
+        System.out.println(nm1);
+        return num*nm1;
+    }
+    
     private static int calculatePower(int num, int pow,int calculate_Value) {
         if(pow==1){
             return calculate_Value=calculate_Value*num; 
